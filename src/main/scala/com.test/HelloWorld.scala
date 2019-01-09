e0c1d51014877f7fa0ec4e0f89c5e6bbbfd24d4d
naiyar_azam <naiyar_azam@apple.com>
2019-01-09 15:51:33 +0530

    plugin scalastyle

--- src/main/scala/com.test/HelloWorld.scala
+++ src/main/scala/com.test/HelloWorld.scala
@@ -3,7 +3,8 @@ package com.test
 object HelloWorld extends App {
 
   override def main(args: Array[String]): Unit = {
-    val glazedDonut = Donut("Glazed Donut", 1111)
-    println("--- "+glazedDonut.product)
+    val CODE = 1111
+    val glazedDonut = Donut("Glazed Donut", productCode = CODE)
+
   }
 }
